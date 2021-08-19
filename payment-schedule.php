60494d33ea364abf8b04d7cec17f12e830097f00
almaruf <maruf.sylhet@gmail.com>
2021-08-19 15:37:46 +0000

    OPTIONS method- return no body

--- payment-schedule.php
+++ payment-schedule.php
@@ -3,6 +3,10 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET");
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("Allow: OPTIONS, GET");
+    exit();
+}
 // 
 // varifying the input 
 //
