0249c330ec9e59399f3a6176dfad94aa363f19a7
almaruf <maruf.sylhet@gmail.com>
2021-08-19 14:13:31 +0000

    getting a list of scheduled dates for future
    based on subscriptionStartDate & scheduleInMonths provided

--- payment-schedule.php
+++ payment-schedule.php
@@ -3,6 +3,9 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET");
 
+// 
+// varifying the input 
+//
 $data = $_GET;
 if (empty($data)
     || !isset($data['scheduleInMonths']) 
@@ -12,10 +15,15 @@ if (empty($data)
     exit();
 }
 
-
 $scheduleInMonths = (int)$data['scheduleInMonths'];
 $subscriptionStartDate = strtotime($data['subscriptionStartDate']);
 
+/*
+ * not processing when scheduleInMonths < 0
+ * not processing when scheduleInMonths > 36
+ * not processing when unacceptable scheduleInMonths date format is provided
+ * not processing when provided scheduleInMonths is in future
+*/
 if ($scheduleInMonths < 0 
     || $scheduleInMonths > 36 
     || strlen($subscriptionStartDate) != 10
@@ -26,8 +34,37 @@ if ($scheduleInMonths < 0
     exit();
 }
 
-// TODO - process here
-
+/* 
+ * get a list of dates for future schedule
+ * based on the subscriptionStartDate and schedulesInMonths
+ */
 $result = [];
+$firstDayOfSubscriptionMonth = strtotime(date('Y-m-01', $subscriptionStartDate));
+$subscriptionDay = date('d', $subscriptionStartDate);
+
+for ($i = 0; $i < $scheduleInMonths; $i++) {
+    // Find out which year is the next month
+    $year = date('Y', strtotime('first day of +1 month', $firstDayOfSubscriptionMonth));
+    
+    // Find out what is the next month
+    $month = date('m', strtotime('first day of +1 month', $firstDayOfSubscriptionMonth));
+    
+    $lastDayOfMonth = date('t', strtotime("$year-$month-01"));
+    $day = $subscriptionDay;
+
+    /*
+     * When subscriptioDay is greater than the last day of the month
+     * for example when subscriptionDay is 31 and the current month 
+     * in consideration is February, we pick the last day of February
+     */
+    if ($subscriptionDay > $lastDayOfMonth) {
+        $day = $lastDayOfMonth;
+    }
+
+    $dateStr = "$year-$month-$day";
+    $firstDayOfSubscriptionMonth = strtotime("$year-$month-01");
+    $result[] = $dateStr;
+}
+
 header('HTTP/1.1 200 OK');
 echo json_encode($result);
