bf5eefd589789ab08410d469436bbb0be4f4a28f
almaruf <maruf.sylhet@gmail.com>
2021-08-19 13:35:10 +0000

    varifying data input
    not processing when scheduleInMonths < 0
    not processing when scheduleInMonths > 36
    not processing when unacceptable scheduleInMonths date format is provided
    not processing when provided scheduleInMonths is in future

--- payment-schedule.php
+++ payment-schedule.php
@@ -4,11 +4,30 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET");
 
 $data = $_GET;
-if (empty($data)) {
-    header("HTTP/1.1 404 Not Found");
+if (empty($data)
+    || !isset($data['scheduleInMonths']) 
+    || !isset($data['subscriptionStartDate'])) {
+
+    header("HTTP/1.1 400 Bad Request");
     exit();
 }
 
-// TODO check valid data
-// prepare data 
-// return correct format etc
+
+$scheduleInMonths = (int)$data['scheduleInMonths'];
+$subscriptionStartDate = strtotime($data['subscriptionStartDate']);
+
+if ($scheduleInMonths < 0 
+    || $scheduleInMonths > 36 
+    || strlen($subscriptionStartDate) != 10
+    || false === $subscriptionStartDate
+    || date('Y-m-d') < date('Y-m-d', $subscriptionStartDate)) {
+
+    header("HTTP/1.1 400 Bad Request");
+    exit();
+}
+
+// TODO - process here
+
+$result = [];
+header('HTTP/1.1 200 OK');
+echo json_encode($result);
